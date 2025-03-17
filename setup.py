a19dd140b61275f344cba3aa03bb750eb45d6d88
n2khanh <n2.khanh.git@gmail.com>
2025-03-17 18:14:56 +0700

    file structure updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medic_Chat',
+    version= '0.0.0',
+    author= 'N@K',
+    author_email= 'kquoc254@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
